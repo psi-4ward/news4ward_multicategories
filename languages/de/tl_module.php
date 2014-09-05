5df4a1a99dcab360a0124d2cafe032eb767b48b6
Christoph Wiechert <wio@psitrax.de>
2014-09-05 13:05:08 +0200

    Added: tl_module translations

---
+++ languages/de/tl_module.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @copyright 4ward.media 2014 <http://www.4wardmedia.de>
+ * @author Christoph Wiechert <wio@psitrax.de>
+ */
+
+$GLOBALS['TL_LANG']['tl_module']['news4ward_categoryFilter'] = array('Kategorie-Filter', 'Nur Beiträge dieser Kategorien anzeigen.');
